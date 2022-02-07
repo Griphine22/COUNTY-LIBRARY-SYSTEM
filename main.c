0d096b540acb7fb8364b21f1d28271df8c67ac34
Griphine22 <98327298+Griphine22@users.noreply.github.com>
2022-02-07 16:59:23 +0300

    7/02/2022 C programming
    Detailed codes for county library

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+void greeting()
+{printf("Welcome Ibrah!\n");.
+
+}
+int main()
+
+{
+    printf("County Library System!\n");
+    greeting();
+    return 0;
+}
+
